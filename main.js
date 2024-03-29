66da4e0813216bb4cf0e6cad3a034ffaaeb37e86
bobjbo <wyattawelch@gmail.com>
2024-03-29 12:48:59 -0500

    made thing auto scroll and made it bigger

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let slideIndex = 1;
 showSlides(slideIndex);
+showSlides2()
 
 // Next/previous controls
 function plusSlides(n) {
@@ -25,4 +26,21 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+function showSlides2() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides2, 10000); // Change image every 2 seconds
 }
